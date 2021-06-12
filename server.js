536c903ab705e698c2b708563eef612b5cc12459
Hamza <toorhamza@gmail.com>
2021-06-12 16:43:08 +0300

    build pipeline

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('build'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
+app.listen(PORT, () => {
+  console.log('server started on port 5000') // eslint-disable-line no-console
+})
